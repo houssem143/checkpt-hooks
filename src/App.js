a78bff16901efeff0573ff9140ae58d1ed0cf0af
houssem143 <houssem14392@gmail.com>
2022-12-13 09:37:00 +0100

    hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { moviesData } from "./data";
+import MovieList from "./Components/MovieList";
+import { useState } from "react";
+import AddMovie from "./Components/AddMovie";
+import Filter from "./Components/Filter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [movies, setMovies] = useState(moviesData);
+  const [text, setText] = useState("")
+  const [rating, setRating] = useState(1)
+
+const handleRating = (x) => setRating(x)
+const handleChange = (e) => setText(e.target.value)
+const handleAdd = (newMovie) => setMovies([...movies,newMovie])
+
+  return <div className="App">
+    <Filter text={text} handleChange={handleChange} rating={rating} handleRating={handleRating} />
+    <MovieList  movies={movies.filter(el=>el.name.toLowerCase().includes(text.toLowerCase())
+      && el.rating >= rating
+      )}  />
+    <AddMovie add={handleAdd} />
+  </div>;
 }
 
 export default App;
